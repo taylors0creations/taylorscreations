a619ad7bd2c880d3403f2236a49e54f6f1049c0f
Taylor <taylors0creations@gmail.com>
2019-07-15 21:54:48 -0400

    Added styled-components, styled header

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,80 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+    background: #ced3dc;
+    align-content: center;
+`;
+
+const NavContainer = styled.div`
+    display: grid;
+    grid-template-columns: 20% 1fr;
+    align-content: center;
+    margin: 0 auto;
+    max-width: 960px;
+    padding: 1.45rem 1.0875rem;
+`;
+
+const LinkContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    opacity: 0.7;
+    list-style-type: none;
+    > li {
+        margin: 0 20px;
+    }
+    a {
+        color: #000;
+    }
+
+    li:last-of-type {
+        color: #fff;
+        border-bottom: 4px solid #fff;
+    }
+`;
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to='/'
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+    <HeaderContainer>
+        <NavContainer>
+            <h1 style={{ margin: 0 }}>
+                <Link
+                    to='/'
+                    style={{
+                        color: `white`,
+                        textDecoration: `none`,
+                    }}
+                >
+                    {siteTitle}
+                </Link>
+            </h1>
+
+            <LinkContainer>
+                <li>
+                    <Link to='/about'>about</Link>
+                </li>
+                <li>
+                    <Link to='/blog'>blog</Link>
+                </li>
+                <li>
+                    <Link to='/shop'>shop</Link>
+                </li>
+                <li>
+                    <Link to='/cart'>cart</Link>
+                </li>
+            </LinkContainer>
+        </NavContainer>
+    </HeaderContainer>
 );
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+    siteTitle: PropTypes.string,
 };
 
 Header.defaultProps = {
-  siteTitle: ``,
+    siteTitle: ``,
 };
 
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,54 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
 import SEO from '../components/seo';
 
+const Container = styled.div`
+    position: relative;
+    max-width: 1500px;
+`;
+
+const HeroContainer = styled.div`
+    background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.4)),
+        url('https://images.unsplash.com/photo-1529651737248-dad5e287768e?ixlib=rb-1.2.1&auto=format&fit=crop&w=2801&q=80');
+    background-size: cover;
+    padding: 4rem 2rem;
+    height: 100vh;
+
+    /* Grid styles */
+    display: grid;
+    align-items: center;
+    justify-items: center;
+`;
+
+const HeroElement = styled.div`
+    /* this is the same as grid-template-columns: 1fr 1fr 1fr 1fr; */
+    grid-template-columns: repeat(4, 1fr);
+    /* this is the same as grid-template-rows: 300px 300px; */
+    grid-template-rows: repeat(2, 300px);
+`;
+
 const IndexPage = () => (
-  <Layout>
-    <SEO title='Home' />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to='/page-2/'>Go to page 2</Link>
-  </Layout>
+    <Layout>
+        <Container>
+            <HeroContainer>
+                <HeroElement>
+                    <h2 style={{ color: 'white', fontSize: 32 }}>
+                        Taylor's Creations
+                    </h2>
+                </HeroElement>
+                <SEO title='Home' />
+                <p style={{ color: 'white', fontSize: 24 }}>
+                    Feel free to buy some thangs.
+                </p>
+                <Link to='/page-2/' style={{ color: 'white', fontSize: 32 }}>
+                    Here's a call to action link.
+                </Link>
+            </HeroContainer>
+        </Container>
+    </Layout>
 );
 
 export default IndexPage;
